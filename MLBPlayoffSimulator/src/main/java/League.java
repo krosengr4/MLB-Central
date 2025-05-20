9c3f031e7dade743c5e9ec0105f37c769aeaa463
krosengr4 <rosenkev4@gmail.com>
2025-05-20 07:32:06 -0500

    Created a League class with constructor, getters, and setters

--- MLBPlayoffSimulator/src/main/java/League.java
+++ MLBPlayoffSimulator/src/main/java/League.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class League {
+
+    String leagueName;
+    String sport;
+    int gamesPerSeason;
+    ArrayList<Team> teams;
+
+    public League() {}
+
+    public League(String leagueName, String sport, int gamesPerSeason) {
+        this.leagueName = leagueName;
+        this.sport = sport;
+        this.gamesPerSeason = gamesPerSeason;
+    }
+
+    //region getters and setters
+    public String getLeagueName() {
+        return leagueName;
+    }
+
+    public void setLeagueName(String leagueName) {
+        this.leagueName = leagueName;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public int getGamesPerSeason() {
+        return gamesPerSeason;
+    }
+
+    public void setGamesPerSeason(int gamesPerSeason) {
+        this.gamesPerSeason = gamesPerSeason;
+    }
+    //endregion
+
+
 }
